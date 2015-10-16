5e61c72a387c1283f5e26617d5f9d4452b77da79
Petr Tsymbarovich <petr@tsymbarovich.ru>
2015-10-16 23:38:10 +0300

    Set version definitions through add_definitions() instead of writing header file

--- Modules/OMZProjectPostConf.cmake
+++ Modules/OMZProjectPostConf.cmake
@@ -8,14 +8,3 @@ add_custom_target(project_files
 ###################### Generated Files #####################
 
 set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${GENERATED_FILES}")
-
-
-########################### CPack ##########################
-
-include(CPack)
-
-
-######################### Versions #########################
-
-file(APPEND ${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}_version.h
-     "#endif // ${CMAKE_PROJECT_NAME_UPPER}_VERSION_H\n")
