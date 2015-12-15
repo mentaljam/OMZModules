2e9d07b14efecea7708e05ad973be7135f7f8151
Petr Tsymbarovich <tpr@ntsomz.ru>
2015-12-15 10:36:15 +0300

    Returned to defining versions through the header file as it's more flexible

--- Modules/OMZProjectPostConf.cmake
+++ Modules/OMZProjectPostConf.cmake
@@ -4,6 +4,11 @@ add_custom_target(project_files
                   COMMENT "Project files"
                   SOURCES ${PROJECT_FILES})
 
+#### Version definitions
+file(APPEND ${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}_version.h
+        "\n#endif // VERSION_${PROJECT_NAME_UPPER}\n"
+)
+
 
 ###################### Generated Files #####################
 
